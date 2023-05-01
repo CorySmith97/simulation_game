4afaea7662a877144f5ec64c194cfb433a91b75a
CorySmith97 <corysmith7991@gmail.com>
2023-05-01 16:51:47 -0600

    Animation_show

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
-        .add_system(sprite_movement)      
+        .add_system(sprite_movement)
+        .add_system(animate_sprite)
         .run();
 }
 
@@ -17,16 +18,24 @@ enum Direction {
     Down,
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>){
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>){
     commands.spawn(Camera2dBundle::default());
+    let texture_handle = asset_server.load("winking_face.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 7, 1, None, None);
+
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    let animation_indices = AnimationIndices { first: 1, last: 6};
     commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("smiley_face.png"),
-            transform: Transform::from_xyz(100., 0., 0.),
-            ..default()
-        },
-        Direction::Up,
-    )); 
+            SpriteSheetBundle {
+                texture_atlas: texture_atlas_handle,
+                sprite: TextureAtlasSprite::new(animation_indices.first),
+                transform: Transform::from_scale(Vec3::splat(3.0)),
+                ..default()
+            },
+            animation_indices,
+            AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+        ));
     
 }
  fn open_map(mut commands: Commands, asset_server: Res<AssetServer>){
@@ -55,6 +64,35 @@ fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, 
     }
 }
 
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
 
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = if sprite.index == indices.last {
+                indices.first
+            } else {
+                sprite.index + 1
+            };
+        }
+    }
+}
 
 
