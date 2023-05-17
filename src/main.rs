9d89366a9d989e32021bb305284dd2c02f68f93f
Zachary Wilson <zachwil99@gmail.com>
2023-05-17 13:45:56 -0600

    ant wandering

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,31 @@
 use bevy::prelude::*;
-mod perlin_noise;
-pub use perlin_noise::*;
 
+pub mod player;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(sprite_movement)
-        .add_system(animate_sprite)
-        .run();
+    .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+    .add_system(player::move_player)
+    .add_startup_system(setup)
+    .run();
 }
 
-#[derive(Component)]
-enum Direction {
-    Up,
-    Down,
-}
-
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut texture_atlases: ResMut<Assets<TextureAtlas>>){
-    commands.spawn(Camera2dBundle::default());
-    let texture_handle = asset_server.load("winking_face.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(64.0, 64.0), 7, 1, None, None);
-
+pub fn setup(mut commands: Commands, asset_server : Res<AssetServer>, mut texture_atlases : ResMut<Assets<TextureAtlas>>)
+{
+    let texture_handle = asset_server.load("ant.png");
+    let texture_atlas = TextureAtlas::from_grid(
+        texture_handle,
+        Vec2::new(64.0,64.0),
+        1,
+        1,
+        Some(Vec2::new(1.,1.)),
+        None,
+    );
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
-
-    let animation_indices = AnimationIndices { first: 1, last: 6};
-    commands.spawn((
-            SpriteSheetBundle {
-                texture_atlas: texture_atlas_handle,
-                sprite: TextureAtlasSprite::new(animation_indices.first),
-                transform: Transform::from_scale(Vec3::splat(3.0)),
-                ..default()
-            },
-            animation_indices,
-            AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
-        ));
-    
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteSheetBundle{
+        texture_atlas:texture_atlas_handle,
+        transform:Transform::from_scale(Vec3::splat(1.0)),
+        ..default()})
+    .insert(player::PlayerMovement{speed:10.});
 }
- fn open_map(mut commands: Commands, asset_server: Res<AssetServer>){
-     make_floor();
-
-    let background_image= asset_server.load("testing.png");
-    commands.spawn(SpriteBundle{
-         texture: background_image,
-         ..default()
-    });
-
- }
-
-
-fn sprite_movement(time: Res<Time>, mut sprite_position: Query<(&mut Direction, &mut Transform)>) {
-    for (mut logo, mut transform) in &mut sprite_position {
-        match *logo {
-            Direction::Up => transform.translation.y += 150. * time.delta_seconds(),
-            Direction::Down => transform.translation.y -= 150. * time.delta_seconds(),
-        }
-        if transform.translation.y > 200. {
-            *logo = Direction::Down;
-        } else if transform.translation.y < -200. {
-            *logo = Direction::Up;
-        }
-    }
-}
-
-#[derive(Component)]
-struct AnimationIndices {
-    first: usize,
-    last: usize,
-}
-
-#[derive(Component, Deref, DerefMut)]
-struct AnimationTimer(Timer);
-
-
-
-fn animate_sprite(
-    time: Res<Time>,
-    mut query: Query<(
-        &AnimationIndices,
-        &mut AnimationTimer,
-        &mut TextureAtlasSprite,
-    )>,
-) {
-    for (indices, mut timer, mut sprite) in &mut query {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            sprite.index = if sprite.index == indices.last {
-                indices.first
-            } else {
-                sprite.index + 1
-            };
-        }
-    }
-}
-
-
