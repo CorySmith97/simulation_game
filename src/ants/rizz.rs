cff4890ee1e5e12db8db76e00d08a5cbbc696b5c
GeoffreyCollins <collinsgeo@byui.edu>
2023-06-12 19:12:12 -0600

    modified: src/ants/rizz.rs

--- src/ants/rizz.rs
+++ src/ants/rizz.rs
@@ -1,37 +1,64 @@
 use rand::{Rng, thread_rng};
 use bevy::prelude::*;
+use std::env;
+
 
 #[derive(Component)]
 pub struct RizzPoints{
+
     pub current_rizz: i32,
 
 }
 
-
 impl RizzPoints {
+
     pub fn new(starting_rizz: i32) -> Self {
+
         RizzPoints {
+
             current_rizz: starting_rizz,
+
         }
     } 
+
     pub fn lose_rizz(current_rizz: i32) -> i32 {
+
         current_rizz - thread_rng().gen_range(5..40)
+
     } 
+
     pub fn gain_rizz(current_rizz: i32) -> i32 {
+
         current_rizz + thread_rng().gen_range(10..40)
+
     }
+
 }
 
 pub fn query_rizz(mut commands: Commands,rizz_points: Query<&mut RizzPoints>) {
+
+    // TODO query is a pub func, query anything that has ants 
+    //     randomly select two ants to have an interaction
     for health in rizz_points.iter() {
-        println!("Rizz points: {}", health.current_rizz);
-    }
-    //{
-      // let rizz_loss = RizzPoints::lose_rizz(current_rizz);
-       //println!("Thanks to your horrible rizz, your rizz points are now {}. Great job.", current_rizz - rizz_loss);
-    //} else
-    //{
-      // let gain_rizz = RizzPoints::gain_rizz(current_rizz);
-      // println!("Thanks to your amazing rizz, your rizz points are now {}. Fantastic job, Rizzler!", current_rizz + gain_rizz);
-    //}
+
+        let random_interaction = thread_rng().gen_range(3..4);
+        let random_ants = thread_rng().gen_range(1..2);
+        if random_ants != 1 {
+            if random_interaction == 3 {
+                
+                let convert_paremeter_to_i32: i32 = health.current_rizz;
+
+                RizzPoints::gain_rizz(convert_paremeter_to_i32);
+                
+            } 
+            else if random_interaction == 4 {
+                
+                let convert_paremeter_to_i32: i32 = health.current_rizz;
+
+                RizzPoints::lose_rizz(convert_paremeter_to_i32);
+            }
+            println!("Rizz points: {}", health.current_rizz);
+
+        }
+    }   
  }
